7f0e49379bc3c90c1b548db0b15099ce04c87cf6
Colin Lyons <colin@tercet.io>
2016-01-21 03:38:32 +0000

    Moving to a single Provider.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,5 @@
 <?php namespace App\Providers;
 
-use BoundedContext\Laravel\Command\Log as CommandLog;
-use BoundedContext\Laravel\Event\Log as EventLog;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,24 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->singleton('BoundedContext\Contracts\Event\Log', function($app)
-        {
-            return new EventLog(
-                $this->app->make('BoundedContext\Contracts\Event\Snapshot\Factory'),
-                $this->app->make('db'),
-                'event_snapshot_log',
-                'event_snapshot_stream'
-            );
-        });
 
-        $this->app->singleton('BoundedContext\Contracts\Command\Log', function($app)
-        {
-            return new CommandLog(
-                $this->app->make('BoundedContext\Contracts\Event\Snapshot\Factory'),
-                $this->app->make('db'),
-                'command_snapshot_log',
-                'command_snapshot_stream'
-            );
-        });
     }
 }
