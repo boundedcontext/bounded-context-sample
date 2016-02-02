e49a23eb49d7e48b256defd988be632cf460de64
Colin Lyons <colin@tercet.io>
2016-02-02 05:29:09 +0000

    Commit.

--- domain/Shopping/Aggregate/Cart/Invariant/HasLessThan10Products/Invariant.php
+++
@@ -1,8 +0,0 @@
-<?php namespace Domain\Shopping\Aggregate\Cart\Invariant\HasLessThan10Products;
-
-use BoundedContext\Contracts\Business\Invariant\Invariant as InvariantContract;
-
-interface Invariant extends InvariantContract
-{
-    /* No need to fully implement this */
-}
