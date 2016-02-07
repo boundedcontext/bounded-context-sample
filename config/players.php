3ca69266ed87745caea5e73607565da060f7f754
Colin Lyons <colin@tercet.io>
2016-02-07 18:44:42 +0000

    Aggregate Stream Builders.

--- config/players.php
+++ config/players.php
@@ -5,11 +5,11 @@ return [
     'application' => [
 
         'projectors' => [
-            '811e5df8-d46e-4b30-8de5-fbfe3790f711' => \App\Projections\Users\Projector::class,
+            //'811e5df8-d46e-4b30-8de5-fbfe3790f711' => \App\Projections\Users\Projector::class,
         ],
 
         'workflows' => [
-            '75bc19be-6137-4050-9205-4eb524c22f8f' => \App\Workflows\Greeting::class,
+            //'75bc19be-6137-4050-9205-4eb524c22f8f' => \App\Workflows\Greeting::class,
             //'5469ca0d-1f26-4e43-8d59-3948457beceb' => \App\Workflows\User::class,
             //'11a7f132-bd84-4a5c-88a7-8d846ff83de7' => \App\Workflows\Another::class,
         ],
@@ -19,8 +19,8 @@ return [
 
         'projectors' => [
 
-            'da54ebc1-1b78-4924-97bf-8463d25aafc8' => \Domain\Test\Invariant\EmailAddressMustBeUnique\Projection\Projector::class,
-            '6eaac031-5bc4-4f4b-aae8-8cb80bab27f9' => \Domain\Test\Invariant\UsernameMustBeUnique\Projection\Projector::class,
+            //'da54ebc1-1b78-4924-97bf-8463d25aafc8' => \Domain\Test\Invariant\EmailAddressMustBeUnique\Projection\Projector::class,
+            //'6eaac031-5bc4-4f4b-aae8-8cb80bab27f9' => \Domain\Test\Invariant\UsernameMustBeUnique\Projection\Projector::class,
         ],
 
         'workflows' => [
--- config/projections.php
+++ config/projections.php
@@ -3,15 +3,15 @@
 return [
 
     'application' => [
+        /*
         \App\Projections\Users\Projection::class =>
             \Infrastructure\App\Projection\Users\Projection::class,
+        */
     ],
 
     'domain' => [
-        \Domain\Test\Invariant\EmailAddressMustBeUnique\Projection\Projection::class =>
-            \Infrastructure\Domain\Projection\EmailAddressMustBeUnique\Projection::class,
 
-        \Domain\Test\Invariant\UsernameMustBeUnique\Projection\Projection::class =>
-            \Infrastructure\Domain\Projection\UsernameMustBeUnique\Projection::class,
+        \Domain\Shopping\Aggregate\Cart\Projection\OnlyActiveMemberCart\Projection::class,
+            Infrastructure\Domain\Shopping\Cart\Projection\OnlyActiveMemberCart\Projection::class,
     ]
 ];
